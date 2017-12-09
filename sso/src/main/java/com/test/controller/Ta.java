37a4ea994044ff18dd689f5742fc939be3c76635
Administrator <18337597808@163.com>
2017-12-09 09:38:53 +0800

    jian()

---
+++ sso/src/main/java/com/test/controller/Ta.java
@@ -0,0 +1,11 @@
+package com.test.controller;
+
+public class Ta {
+    public int sum(int x, int y){
+        return  x+y;
+    }
+
+    public int jian(int x,int y){
+        return  x-y;
+    }
+}
